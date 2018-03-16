92d34bc438b9e3939b718fe78d21ce0a515662a2
is.quicano.2017 <is.quicano.2017@alumnos.urjc.es>
2018-03-16 16:24:45 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+a=1
+b=0
+
+print("Hello from Pycharm 0")
+print("Hello from Pycharm 1")
+print("Hello from Pycharm 2")
+print("Hello from Pycharm 3")
+
+
+print("Hello from Pycharm 4")
+print("Hello from Pycharm 5")
+print("Hello from Pycharm 6")
+print("Hello from Pycharm 7")
+print("Hello from Pycharm 8")
+print("Hello from Pycharm 9")
+
