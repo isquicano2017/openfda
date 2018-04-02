a498a8745c3db672c58ecda8652462a6b7fd330e
is.quicano.2017 <is.quicano.2017@alumnos.urjc.es>
2018-04-02 23:33:38 +0200

    Second commit

--- openfda-1/openfda-1.2.py
+++ openfda-1/openfda-1.2.py
@@ -15,5 +15,5 @@ repo=repos1['results']
 #drugs=json.loads["drugs_raw"]["results"]
 
 for i in range (0,10):
-    print("The id of the following drug:", i+1,"is", repo[0:10], ["id"])
+    print("The id of the following drug:", i+0,"is", repo[0:10], ["id"])
       #  print(["spl_id"])
