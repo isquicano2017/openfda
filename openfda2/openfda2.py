609ee3d46b6746ba8c57793641638684069d8d9f
is.quicano.2017 <is.quicano.2017@alumnos.urjc.es>
2018-04-03 12:01:09 +0200

    First commit

--- openfda2/openfda2.py
+++ openfda2/openfda2.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json", None, headers)
+conn.request("GET", "/drug/label.json?search=salycilic", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 repos_raw = r1.read().decode("utf-8")
