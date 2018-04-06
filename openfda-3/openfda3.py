9d3b92a7a6cb9d9039faebf4358c63ce851db031
isquicano2017 <is.quicano.2017@alumnos.urjc.es>
2018-04-06 15:59:20 +0200

    Commit

--- openfda-3/openfda3.py
+++ openfda-3/openfda3.py
@@ -16,6 +16,17 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
 
+        headers = {'User-Agent': 'http-client'}
+
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET", "/drug/label.json", None, headers)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        repos_raw = r1.read().decode("utf-8")
+        conn.close()
+
+        repos1 = json.loads(repos_raw)
+
         # Send message back to client
         message = "Hello world! " + self.path
         # Write content as utf-8 data
