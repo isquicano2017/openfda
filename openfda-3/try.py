66a62638ae40a3929bb9f36206281af27c096720
is.quicano.2017 <is.quicano.2017@alumnos.urjc.es>
2018-05-12 15:02:57 +0200

    commit

--- openfda-3/try.py
+++ openfda-3/try.py
@@ -33,14 +33,14 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             conn.close()
 
             with open("html_file_2.html", "w"):
-                self.wfile.write(bytes('<html><head> <h2><font face= "Lucida Handwriting">Search OpenFDA</font></h2><h2>Open FDA first web </h2><h2>Drug Label<input name="label"></h2><input type = "submit"><p><img src ="https://media.giphy.com/media/zk8myPBS6zuWQ/giphy.gif"></p>', "utf8"))
+                self.wfile.write(bytes('<html><head> <h2><font face= "Lucida Handwriting">Search OpenFDA</font></h2><h2>Open FDA first web </h2><h2>Drug Label<input name="label"></h2><input type = "submit">', "utf8"))
                 for i in range(len(repos['results'])):
                     try:
                         drug = '<li>' + repos['results'][i]["openfda"]["generic_name"][0] + '</li>'
                         self.wfile.write(bytes(drug, "utf8"))
                     except KeyError:
                         continue
-                self.wfile.write(bytes('</ol><h3>Thank you, come again</h3> \n <img src="http://www.konbini.com/en/files/2017/08/apu-feat.jpg" alt="Sad"><p><a href="http://%s:%s/">Back to Main Page</a></p></head></html>' % (IP, PORT), "utf8"))
+                self.wfile.write(bytes('</ol><h3>Thank you. Hope to see you soon</h3> \n <p><img src ="https://media.giphy.com/media/zk8myPBS6zuWQ/giphy.gif"></p><a href="http://%s:%s/">Back to Main Page</a></p></head></html>' % (IP, PORT), "utf8"))
 
         path = self.path
             # Send message back to client
