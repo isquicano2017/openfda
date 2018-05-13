6324df9e86338dc1f1c3c0102273f78371731509
isquicano2017 <is.quicano.2017@alumnos.urjc.es>
2018-05-13 17:36:30 +0200

    Working on it.

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -8,10 +8,22 @@ PORT= 9006
 socketserver.TCPServer.allow_reuse_address= True
 
 class OpenFDAClient():
-    def active_communication(self, drug, limit):
+    def inform_communication(self, drug, limit):
         headers= {'User-Agent': 'http-client'}
         conn= http.client.HTTPSConnection("api.fda.gov")
         url=/drug/label.json?search=active_ingredient:" + drug + "&" + "limit=" + limit
+        conn.request("GET", url, None, headers)
+        r1 = conn.getresponse()
+        drugs_raw = r1.read().decode("utf-8")
+        conn.close()
+        drug = json.loads(drugs_raw)
+        drugs_1 = drug
+        return drugs_1
+    def inform_company (self, drug, limit):
+        headers = {'User-Agent': 'http-client'}
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        url = / drug / label.json?search = manufacturer_name:" + drug + " & " + "
+        limit = " + limit
         conn.request("GET", url, None, headers)
         r1 = conn.getresponse()
         drugs_raw = r1.read().decode("utf-8")
